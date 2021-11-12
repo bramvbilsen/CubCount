a936095de3a79e1701a8ead014d3e7b74b347859
Geert Wesemael <onlinegeert@gmail.com>
2021-11-12 16:09:51 +0100

    voxilizedMesh

--- Assets/voxelizor.cs
+++ Assets/voxelizor.cs
@@ -8,16 +8,6 @@ public class voxelizor : MonoBehaviour
 {
 
     //VOXELIZATION
-    public List<Vector3Int> GridPoints = new List<Vector3Int>();
-    public float HalfSize = 0.1f;
-    public Vector3 LocalOrigin;
-
-    public Vector3 PointToPosition(Vector3Int point)
-    {
-        float size = HalfSize * 2f;
-        Vector3 pos = new Vector3(HalfSize + point.x * size, HalfSize + point.y * size, HalfSize + point.z * size);
-        return LocalOrigin + transform.TransformPoint(pos);
-    }
     public static void VoxelizeMesh(MeshFilter meshFilter)
     {
         if (!meshFilter.TryGetComponent(out MeshCollider meshCollider))
