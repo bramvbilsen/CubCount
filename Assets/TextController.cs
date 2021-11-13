e0745fb08b65ea9c553a05265a761daa89eee7b8
Ilias Swinnen <swinnen.ilias@gmail.com>
2021-11-13 11:16:02 +0100

    Working number buttons

---
+++ Assets/TextController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour
+{
+
+    public GameObject txt;
+
+    public void SetText(string s){
+        Text guessText = txt.GetComponent<Text>();
+        guessText.text += s;
+    }
+
+    public void Backspace(){
+        Text guessText = txt.GetComponent<Text>();
+        guessText.text = guessText.text.Remove(guessText.text.Length-1);
+    }
+
+    public void Clear(){
+        Text guessText = txt.GetComponent<Text>();
+        guessText.text = "";
+    }
+
+    public void Submit(){
+        Debug.Log("Submitting guess");
+    }
+ 
+}
+
+    
